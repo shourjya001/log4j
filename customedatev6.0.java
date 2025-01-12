da916d9495ff25b87b50c2b5ed517c8dbc9e7a7f
shourjya001 <83701525+shourjya001@users.noreply.github.com>
2025-01-12 13:11:16 +0530

    Create customedatev6.0.java

---
+++ customedatev6.0.java
@@ -0,0 +1,58 @@
+package com.socgen.dbe;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.helpers.LogLog;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class CustomRealTimeAppender extends FileAppender {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    private String lastFileName;
+    private boolean appendToFile = true;
+    
+    public void setAppendToFile(boolean append) {
+        this.appendToFile = append;
+    }
+    
+    @Override
+    public void activateOptions() {
+        updateFileName();
+        super.activateOptions();
+    }
+    
+    private void updateFileName() {
+        try {
+            String date = dateFormat.format(new Date());
+            File logsDir = new File("logs");
+            if (!logsDir.exists()) {
+                logsDir.mkdirs();
+            }
+            
+            // Changed filename format to maestrologs_date.log
+            String newFileName = "logs/maestrologs_" + date + ".log";
+            
+            if (!newFileName.equals(lastFileName)) {
+                lastFileName = newFileName;
+                setFile(newFileName, appendToFile, bufferedIO, bufferSize);
+            }
+        } catch (IOException e) {
+            LogLog.error("Error updating log file", e);
+        }
+    }
+    
+    @Override
+    protected void subAppend(org.apache.log4j.spi.LoggingEvent event) {
+        String currentDate = dateFormat.format(new Date());
+        String expectedFileName = "logs/maestrologs_" + currentDate + ".log";
+        
+        if (!expectedFileName.equals(lastFileName)) {
+            updateFileName();
+        }
+        
+        super.subAppend(event);
+        if (qw != null) {
+            qw.flush();
+        }
+    }
+}
